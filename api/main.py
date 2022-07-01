0e18ed402dffe555893251840b9b878568d7b394
davbrican <72500941+davbrican@users.noreply.github.com>
2022-07-01 10:53:50 +0200

    a

--- api/main.py
+++ api/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from datetime import datetime, timedelta
 import pandas as pd                        
 from pytrends.request import TrendReq
+import re
 
 app = FastAPI()
 
@@ -14,7 +15,7 @@ pytrend = TrendReq()
 
 
 
-def main_search(sex, race, year):
+def life_expectancy_search(sex, race, year):
     uri = "https://data.cdc.gov/resource/w9j2-ggv5.json?"
     if race:
         uri += "&race=" + race
@@ -25,23 +26,9 @@ def main_search(sex, race, year):
     result = []
     with urllib.request.urlopen(uri) as url:
         data = json.loads(url.read().decode())
-        #result = search_in_data(data, sex, race, year)
         return data
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/life_expectancy/{sex}/{race}/{year}")
-async def life_expectancy(sex: str, race: str, year: str):
-    search = main_search(sex, race, year)
-    try:
-        return {"average_life_expectancy": sum(float(d['average_life_expectancy']) for d in search) / len(search)}
-    except:
-        return {"error": "Without Data"}
-
-@app.get("/unemployment/{state}")
-async def unemployment(state: str):
+    
+def unemployment_search():
     uri = "https://www.bls.gov/web/laus/lauhsthl.htm"
     response = urllib.request.urlopen(uri)
     soup = BeautifulSoup(response, 'lxml')
@@ -49,21 +36,26 @@ async def unemployment(state: str):
     result_dict = {}
     for row in table:
         result_dict[row.find("th").string] = row.find_all("td")[2].string
-    if state in result_dict:
-        return {"rate": result_dict[state]}
-    else:
-        return {"error": "State not found"}
-
-@app.get("/trends")
-async def trends(phrase: str = "", start_date: str = "", end_date: str = ""):
+        
+    return result_dict
+    
+def trends_search(phrase, start_date, end_date):
     N_DAYS_AGO = 14
     today = datetime.now()    
     n_days_ago = today - timedelta(days=N_DAYS_AGO)
+    
     if start_date == "": start_date = n_days_ago
-    else: start_date = datetime.strptime(start_date, "%Y-%m-%d")
+    else:
+        try:
+            start_date = datetime.strptime(start_date, "%Y-%m-%d")
+        except:
+            return None
     if end_date == "": end_date = today
-    else: end_date = datetime.strptime(end_date, "%Y-%m-%d")
-        
+    else:
+        try:
+            end_date = datetime.strptime(end_date, "%Y-%m-%d")
+        except:
+            return None
     if phrase != "":
         kw_list=[phrase]
         time_frame = ''
@@ -73,4 +65,34 @@ async def trends(phrase: str = "", start_date: str = "", end_date: str = ""):
             result.append(int(row[0]))
         return {"interest": result}
     else:
-        return {"error": "A phrase is required"}
+        return None
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/life_expectancy/{sex}/{race}/{year}")
+async def life_expectancy(sex: str, race: str, year: str):
+    search = life_expectancy_search(sex, race, year)
+    try:
+        return {"average_life_expectancy": sum(float(d['average_life_expectancy']) for d in search) / len(search)}
+    except:
+        return {"error": "Without Data"}
+
+@app.get("/unemployment/{state}")
+async def unemployment(state: str):
+    result_dict = unemployment_search()
+    if state in result_dict:
+        return {"rate": result_dict[state]}
+    else:
+        return {"error": "State not found"}
+
+@app.get("/trends")
+async def trends(phrase: str = "", start_date: str = "", end_date: str = ""):
+    result = trends_search(phrase, start_date, end_date)
+    if result != None:
+        return result
+    else:
+        return {"error": "A phrase is required or dates are badformed (Correct way: YYYY-mm-dd)"}

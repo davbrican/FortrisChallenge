a61ab2c3533a80b2ac621345b183d419d4c16398
davbrican <72500941+davbrican@users.noreply.github.com>
2022-07-01 14:54:14 +0200

    a

---
+++ api/Readme.md
@@ -0,0 +1,37 @@
+# Python Challenge API
+This is an API to get a great sort of information about different kind of data
+
+### GETTERS
+**_/_**
+>Welcome endpoint with no useful information
+
+**_/life_expectancy/{sex}/{race}/{year}_**
+>A life expectancy getter. It returns the avarage of life expectancy depending of some factors given in the parameters of the request. Those parameteres are:
+-sex: Including just "Male", "Female" and "Both sexes"
+-race: Including just "Black", "White" and "All"
+-year: Where the year must be 1900 <= year <= 2018
+
+Example: http://API_URL/life_expectancy/Male/White/1800
+
+**_/unemployment/{state}_**
+>A rate value of the unemployment of the specified state from USA
+
+Example: http://API_URL/unemployment/Alabama
+
+**_/trends_**
+>A hourly global trends of a given phrase. To use this get, the phrase must be specified in the query and the start and end date can be specified too (but those 2 are optionals):
+-phrase: A text to be searched
+-start_date: A date with the format YYYY-mm-dd
+-end_date: A date with the format YYYY-mm-dd
+
+Example: http://API_URL/trends?phrase=hello%20world&start_date=2022-06-20&end_date=2022-06-25
+
+**_/weather_**
+>Returns the climate in the last 7 days in your location
+
+Example: http://API_URL/weather
+
+**_/trends_weather_**
+>Returns the climate in your location and a phrase given trends in the last 7 days
+
+Example: http://API_URL/trends_weather?phrase=hello%20world
--- api/main.py
+++ api/main.py
@@ -7,29 +7,32 @@ from pytrends.request import TrendReq
 import xml.etree.ElementTree as ET
 import re
 import requests
+import geocoder
+import os
 
 app = FastAPI()
 
-from pytrends.request import TrendReq
-
-# Only need to run this once, the rest of requests will use the same session.
 pytrend = TrendReq()
 
 
+# Auxiliar functions
 
 def life_expectancy_search(sex, race, year):
     uri = "https://data.cdc.gov/resource/w9j2-ggv5.json?"
     if race:
+        if race not in ["Black", "White", "All"]:
+            return None
         uri += "&race=" + race
     if sex:
-        uri += "&sex=" + sex
+        if sex not in ["Male", "Female", "Both sexes"]:
+            return None
+        uri += "&sex=" + sex.replace(" ", "%20")
     if year:
         uri += "&year=" + year
-    result = []
     with urllib.request.urlopen(uri) as url:
         data = json.loads(url.read().decode())
         return data
-    
+
 def unemployment_search():
     uri = "https://www.bls.gov/web/laus/lauhsthl.htm"
     response = urllib.request.urlopen(uri)
@@ -40,7 +43,7 @@ def unemployment_search():
         result_dict[row.find("th").string] = row.find_all("td")[2].string
         
     return result_dict
-    
+
 def trends_search(phrase, start_date, end_date):
     N_DAYS_AGO = 14
     today = datetime.now()    
@@ -69,13 +72,14 @@ def trends_search(phrase, start_date, end_date):
     else:
         return None
 
-def weather_search():
+def weather_search(location, with_trends=False, phrase=""):
     today = datetime.now()
     resulting_dict = {}
     for day_index in range(7):
         d = today - timedelta(days=day_index)
-        key = "0e8b6d7ec08b49bbb8b85340220107"
-        uri = "http://api.weatherapi.com/v1/history.xml?key=" + key + "&q=Sevilla&dt=" + str(d.strftime("%Y-%m-%d"))
+        key = os.environ['TOKEN']
+            
+        uri = "http://api.weatherapi.com/v1/history.xml?key=" + key + "&q=" + str(location) + "&dt=" + str(d.strftime("%Y-%m-%d"))
         response = requests.get(uri).text
         
         tree = ET.fromstring(response)
@@ -114,8 +118,16 @@ def weather_search():
 
         day = forecastday[0].text
 
-        resulting_dict["days"].append({day: {"temperature": hourly_temperature, "pressure": hourly_pressure, "wind": hourly_wind_speed, "precipitations": hourly_precipitation, "humidity": hourly_humidity, "condition_history": condition_history}})
+        
+        if with_trends:
+            interest_result = trends_search(phrase=phrase, start_date=d.strftime("%Y-%m-%d"), end_date=d.strftime("%Y-%m-%d"))
+            resulting_dict["days"].append({"date": day, "interest": interest_result["interest"][0], "weather": {"temperature": hourly_temperature, "pressure": hourly_pressure, "wind": hourly_wind_speed, "precipitations": hourly_precipitation, "humidity": hourly_humidity, "condition_history": condition_history}})
+        else:
+            resulting_dict["days"].append({day: {"temperature": hourly_temperature, "pressure": hourly_pressure, "wind": hourly_wind_speed, "precipitations": hourly_precipitation, "humidity": hourly_humidity, "condition_history": condition_history}})
     return resulting_dict
+
+
+# API endpoints
 
 @app.get("/")
 async def root():
@@ -124,6 +136,8 @@ async def root():
 @app.get("/life_expectancy/{sex}/{race}/{year}")
 async def life_expectancy(sex: str, race: str, year: str):
     search = life_expectancy_search(sex, race, year)
+    if search == None:
+        return {"error": "Sex or Race are not included in the available ones"}
     try:
         return {"average_life_expectancy": sum(float(d['average_life_expectancy']) for d in search) / len(search)}
     except:
@@ -144,7 +158,16 @@ async def trends(phrase: str = "", start_date: str = "", end_date: str = ""):
         return result
     else:
         return {"error": "A phrase is required or dates are badformed (Correct way: YYYY-mm-dd)"}
-    
+
 @app.get("/weather")
 async def weather():
-    return {"weather": weather_search()}
+    my_location = geocoder.ip('me')
+    return {"weather": weather_search(my_location)}
+
+@app.get("/trends_weather")
+async def trends_weather(phrase: str = ""):
+    my_location = geocoder.ip('me')
+    if phrase != "":
+        return weather_search(my_location, True, phrase)
+    else:
+        return {"error": "A phrase is required"}

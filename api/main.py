72c56f1f7ebf5b6d88af47f8cfd4acb1691ca962
davbrican <72500941+davbrican@users.noreply.github.com>
2022-07-05 16:08:23 +0200

    a

--- api/main.py
+++ api/main.py
@@ -43,7 +43,7 @@ def unemployment_search():
     table = soup.find("table", {"class": "regular"}).find("tbody").find_all("tr")
     result_dict = {}
     for row in table:
-        result_dict[row.find("th").string] = row.find_all("td")[2].string
+        result_dict[row.find("th").string.lower()] = row.find_all("td")[0].string
         
     return result_dict
 
@@ -52,32 +52,23 @@ def trends_search(phrase, start_date, end_date):
     today = datetime.now()    
     n_days_ago = today - timedelta(days=N_DAYS_AGO)
     
-    if start_date == "": start_date = n_days_ago
-    else:
-        try:
-            start_date = datetime.strptime(start_date, "%Y-%m-%d")
-        except:
-            return None
-    if end_date == "": end_date = today
-    else:
-        try:
-            end_date = datetime.strptime(end_date, "%Y-%m-%d")
-        except:
-            return None
+    try:
+        if start_date == "": start_date = n_days_ago
+        else: start_date = datetime.strptime(start_date, "%Y-%m-%d")
+        if end_date == "": end_date = today
+        else: end_date = datetime.strptime(end_date, "%Y-%m-%d")
+    except:
+        return None
+    
     if phrase != "":
-        kw_list=[phrase]
-        time_frame = ''
-        regiondf = pytrend.get_historical_interest(kw_list, year_start=start_date.year, month_start=start_date.month, day_start=start_date.day, hour_start=0, year_end=end_date.year, month_end=end_date.month, day_end=end_date.day, hour_end=0, cat=0, geo='', gprop='', sleep=0)
-        result = []
-        for index, row in regiondf.iterrows():
-            result.append(int(row[0]))
-        return {"interest": result}
+        regiondf = pytrend.get_historical_interest([phrase], year_start=start_date.year, month_start=start_date.month, day_start=start_date.day, hour_start=0, year_end=end_date.year, month_end=end_date.month, day_end=end_date.day, hour_end=0, cat=0, geo='', gprop='', sleep=0)
+        return {"interest": [int(row[0]) for index, row in regiondf.iterrows()]}
     else:
         return None
 
 def weather_search(location, with_trends=False, phrase=""):
     today = datetime.now()
-    resulting_dict = {}
+    resulting_dict = {"city": str(location), "days": []}
     for day_index in range(7):
         d = today - timedelta(days=day_index)
         key = os.environ['TOKEN']
@@ -86,44 +77,22 @@ def weather_search(location, with_trends=False, phrase=""):
         response = requests.get(uri).text
         
         tree = ET.fromstring(response)
-        location = tree[0]
+        resulting_dict["city"] = tree[0][0].text
         forecastday = tree[1][0]
-        if "city" not in resulting_dict:
-            resulting_dict["city"] = location[0].text
-        if "days" not in resulting_dict:
-            resulting_dict["days"] = []
 
-        hourly_temperature = 0
-        hourly_pressure = 0
-        hourly_wind_speed = 0
-        hourly_humidity = 0
-        hourly_precipitation = 0
-        condition_history = []
-        hours = 0
-
-        for i in forecastday:
-            if i.tag == "hour":
-                hourly_temperature += float(i[2].text)
-                hourly_pressure += float(i[10].text)
-                hourly_wind_speed += float(i[7].text)
-                hourly_humidity += float(i[14].text)
-                hourly_precipitation += float(i[12].text)
-                conditions = i[5]
-                condition_history.append(conditions[0].text)
-                hours += 1
-                
-        hourly_temperature = round((hourly_temperature / hours), 2)
-        hourly_pressure = round((hourly_pressure / hours), 2)
-        hourly_wind_speed = round((hourly_wind_speed / hours), 2)
-        hourly_humidity = round((hourly_humidity / hours), 2)
-        hourly_precipitation = round((hourly_precipitation / hours), 2)
-
+        hourly_temperature = round(sum([float(i[2].text) for i in forecastday if i.tag == "hour"]) / 24, 2)
+        hourly_pressure = round(sum([float(i[10].text) for i in forecastday if i.tag == "hour"]) / 24, 2)
+        hourly_wind_speed = round(sum([float(i[7].text) for i in forecastday if i.tag == "hour"]) / 24, 2)
+        hourly_humidity = round(sum([float(i[14].text) for i in forecastday if i.tag == "hour"]) / 24, 2)
+        hourly_precipitation = round(sum([float(i[12].text) for i in forecastday if i.tag == "hour"]) / 24, 2)
+        condition_history = [i[5][0].text for i in forecastday if i.tag == "hour"]
 
         day = forecastday[0].text
 
         
         if with_trends:
             interest_result = trends_search(phrase=phrase, start_date=d.strftime("%Y-%m-%d"), end_date=d.strftime("%Y-%m-%d"))
+            if len(interest_result["interest"]) == 0: interest_result["interest"].append("Without data")
             resulting_dict["days"].append({"date": day, "interest": interest_result["interest"][0], "weather": {"temperature": hourly_temperature, "pressure": hourly_pressure, "wind": hourly_wind_speed, "precipitations": hourly_precipitation, "humidity": hourly_humidity, "condition_history": condition_history}})
         else:
             resulting_dict["days"].append({day: {"temperature": hourly_temperature, "pressure": hourly_pressure, "wind": hourly_wind_speed, "precipitations": hourly_precipitation, "humidity": hourly_humidity, "condition_history": condition_history}})
@@ -142,15 +111,15 @@ async def life_expectancy(sex: str, race: str, year: str):
     if search == None:
         return {"error": "Sex or Race are not included in the available ones"}
     try:
-        return {"average_life_expectancy": sum(float(d['average_life_expectancy']) for d in search) / len(search)}
+        return {"average_life_expectancy": float(search[0]["average_life_expectancy"])}
     except:
         return {"error": "Without Data"}
 
 @app.get("/unemployment/{state}")
 async def unemployment(state: str):
     result_dict = unemployment_search()
-    if state in result_dict:
-        return {"rate": float(result_dict[state])}
+    if state.lower() in result_dict:
+        return {"rate": float(result_dict[state.lower()])}
     else:
         return {"error": "State not found"}
 
